e9084895bef9c83728f5944a0d644b7ee6e4a8b6
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-04-28 01:56:05 +0900

    implement CUD of book

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'book/index'
-  get 'book/edit'
-  get 'book/show'
-  get 'book/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root to: 'books#index'
 end
