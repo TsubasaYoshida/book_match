8cc96166b6406e6dc353c868e2ba9ee59d138368
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-04-27 23:58:23 +0900

    create books_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'book/index'
+  get 'book/edit'
+  get 'book/show'
+  get 'book/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
