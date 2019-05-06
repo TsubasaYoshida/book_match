24452ddfe38d4a413bbcd03985b4e6083461c988
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-05-06 21:10:12 +0900

    set timezone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ module BookMatch
   class Application < Rails::Application
     config.load_defaults 5.2
 
+    # タイムゾーン指定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       # 「rails g」時の自動生成を制御
       g.assets false
